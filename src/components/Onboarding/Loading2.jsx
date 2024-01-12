79ea9f561a9f47a3c1b0209f12770b80d4ab12cd
Ronald Kelechi <ronaldkelechi11@gmail.com>
2024-01-12 05:06:47 +0100

    importing instead of directly from code

--- src/components/Onboarding/Loading2.jsx
+++ src/components/Onboarding/Loading2.jsx
@@ -1,14 +1,16 @@
-// import Loader from "../utils/loader"
+import Loader from "../../components/utils/Loader";
+import groupImage from '../../assets/images/loading2group.png'
+
 
 
 
 const Loading2 = () => {
     return (
         <div className='w-screen h-screen bg-black flex flex-col'>
-            <img src="/assets/images/loading2group.png" className="h-[50%] w-full" alt="" />
+            <img src={groupImage} className="h-[50%] w-full" alt="" />
 
             <div className="h-[50%] w-full">
-                {/* <Loader section={2} /> */}
+                <Loader section={2} />
             </div>
         </div>
     )
