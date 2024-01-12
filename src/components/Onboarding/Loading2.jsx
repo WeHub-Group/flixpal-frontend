3491c1fe3a4cc41285d5fce58b3c3f726c7b7426
Ronald Kelechi <ronaldkelechi11@gmail.com>
2024-01-12 10:42:09 +0100

    Almost done with the UI for Onboarding

--- src/components/Onboarding/Loading2.jsx
+++ src/components/Onboarding/Loading2.jsx
@@ -1,14 +1,36 @@
+<<<<<<< Updated upstream
 // import Loader from "../utils/loader"
 
+=======
+import Loader from "../../components/utils/Loader";
+import groupImage from '../../assets/images/loading2group.png'
+import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
+import CustomButton from "../utils/CustomButton";
+>>>>>>> Stashed changes
 
 
 const Loading2 = () => {
+    const navigate = useNavigate()
+
+
     return (
         <div className='w-screen h-screen bg-black flex flex-col'>
             <img src="/assets/images/loading2group.png" className="h-[50%] w-full" alt="" />
 
+<<<<<<< Updated upstream
             <div className="h-[50%] w-full">
                 {/* <Loader section={2} /> */}
+=======
+            <div className="h-[55%] w-full flex flex-col justify-evenly gap-2 p-4">
+                <Loader section={2} />
+                <p className="text-white  text-4xl text-center font-[1100]">
+                    Limitless access <br /> to movies, TV <br /> shows, and <br /> beyond.
+                </p>
+                <p className="text-[#797979]  text-sm text-center">Stream Flixpal from any location. <br />    Cancel anytime.</p>
+
+                <CustomButton text={'Next'} onPressed={() => { navigate('/3') }} />
+>>>>>>> Stashed changes
             </div>
         </div>
     )
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Loading from './components/Onboarding/Loading'
 import Loading2 from './components/Onboarding/Loading2'
+<<<<<<< Updated upstream
+=======
+import Loading3 from './components/Onboarding/Loading3'
+import Loading4 from './components/Onboarding/Loading4'
+
+>>>>>>> Stashed changes
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -15,6 +21,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Route path='/'>
         <Route index element={<Loading />} />
         <Route path='2' element={<Loading2 />} />
+<<<<<<< Updated upstream
+=======
+        <Route path='3' element={<Loading3 />} />
+        <Route path='4' element={<Loading4 />} />
+>>>>>>> Stashed changes
       </Route>
 
 
