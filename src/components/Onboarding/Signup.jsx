3acc433a3c8219d22fb610c1f31597c73295e1de
Ronald Kelechi <ronaldkelechi11@gmail.com>
2024-01-12 18:42:23 +0100

    Working on Signup

---
+++ src/components/Onboarding/Signup.jsx
@@ -0,0 +1,12 @@
+import BackButton from "../../components/utils/BackButton";
+
+
+const SignUp = () => {
+    return (
+        <div className="w-screen h-screen bg-black flex justify-content items-center p-5">
+            <BackButton />
+        </div>
+    )
+}
+
+export default SignUp
