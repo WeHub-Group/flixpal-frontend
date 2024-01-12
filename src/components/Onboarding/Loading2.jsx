287e233927da655e0069c9cfd05659fd46f3add7
Ronald Kelechi <ronaldkelechi11@gmail.com>
2024-01-12 04:55:18 +0100

    moved images to public

--- src/components/Onboarding/Loading2.jsx
+++ src/components/Onboarding/Loading2.jsx
@@ -5,7 +5,7 @@
 const Loading2 = () => {
     return (
         <div className='w-screen h-screen bg-black flex flex-col'>
-            <img src="/src/assets/images/loading2group.png" className="h-[50%] w-full" alt="" />
+            <img src="/assets/images/loading2group.png" className="h-[50%] w-full" alt="" />
 
             <div className="h-[50%] w-full">
                 {/* <Loader section={2} /> */}
