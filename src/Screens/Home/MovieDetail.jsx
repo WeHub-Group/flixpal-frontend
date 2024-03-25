0f41400f1e8461f67433cb938b0f8b2fceef5f44
Ronald Kelechi <ronaldkelechi11@gmail.com>
2024-03-25 01:14:41 +0100

    Holding to solve username save issues

--- src/Screens/Home/MovieDetail.jsx
+++ src/Screens/Home/MovieDetail.jsx
@@ -4,21 +4,8 @@ import Navbar from "../../utils/reuseable/Navbar"
 import { useEffect, useState } from "react"
 import axios from "axios"
 
-const MovieDetail = () => {
+const MovieDetail = ({ movie }) => {
     const { moviename } = useParams()
-    const [movie, setMovie] = useState({})
-
-    useEffect(() => {
-        // Trying to fetch from the json file
-        axios.get(import.meta.env.VITE_BACKEND_URL)
-            .then(async (result) => {
-                // Filter out that particular movie
-                await setMovie(result.data)
-            }).catch((err) => {
-                console.log(err);
-            });
-    }, [])
-
 
     return (
         <ScrollToLeft classname='bg-black h-screen w-screen'>
--- src/utils/reuseable/Movie.jsx
+++ src/utils/reuseable/Movie.jsx
@@ -1,13 +1,17 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom"
+import MovieDetail from "../../Screens/Home/MovieDetail"
 
 const Movie = ({ movie }) => {
 
+    // OnClick open the movie detail page and pass info through the params
+
+
     return (
-        <Link to={movie.name} className='w-24 min-w-24 h-full flex flex-col gap-2 rounded-lg'>
+        <div className='w-24 min-w-24 h-full flex flex-col gap-2 rounded-lg'>
             <img src={movie.cover} className="h-full w-24" />
             <p className="text-grey text-sm text-center">{movie.name}</p>
-        </Link>
+        </div>
     )
 }
 
